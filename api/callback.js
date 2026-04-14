8d0160d67a9c3456d0aa0303314a2b1cfaa323e8
turcaiustin2007-sys <turcaiustin2007@gmail.com>
2026-04-14 14:55:56 +0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
             projectId:   process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             privateKey:  process.env.FIREBASE_PRIVATE_KEY
-                ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+                ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"|"$/g, '')
                 : undefined
         })
     });
